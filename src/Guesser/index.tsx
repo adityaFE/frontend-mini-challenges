0b60f7b923460dd481c589585b3006db9d137cb6
Aditya Anand <adityaanandofficial@gmail.com>
2024-09-29 23:13:51 +0530

    adding guesser component

---
+++ src/Guesser/index.tsx
@@ -0,0 +1,70 @@
+import {useState} from 'react'
+import {GusserWrapper, Input, Label, SubmitButton, SubmitButtonWrapper} from './subcomponents'
+
+const random = Math.floor((Math.random())*100)
+const Gusser = () => {
+
+    const [formData,setFormData]= useState<number>(0)
+
+    const [text,setText] = useState<string>('Start Guessing')
+
+    const [count,setCount] = useState<number>(0)
+
+    const [previousGuess,setPreviousGuess]= useState<null| number>(null)
+
+    const submitHandler= (event:React.FormEvent<HTMLFormElement>) =>{
+                        event.preventDefault()
+
+            const data= formData
+            setPreviousGuess(data)
+            if(data>random){
+                setText('Guess Lower')
+                setCount(count+1)
+                setFormData(0)
+            }
+            else if (data<random){
+                setText('Guess Higher')
+                setCount(count+1)
+                setFormData(0)
+            }
+            else{
+                setText('Congratulations')
+            }
+        }
+
+    const handleChange= (event:React.ChangeEvent<HTMLInputElement>) =>{
+        setFormData(Number(event.target.value))
+    }
+
+    const handleReset= () =>{
+    setFormData(1)
+    setText('Start Guessing')
+    setCount(0)
+    setPreviousGuess(null)
+    }
+
+ return (
+    <GusserWrapper>
+        <form onSubmit={submitHandler}>
+            <Label htmlFor='random-text'>Guess a number from 1 to 100</Label>
+            <br></br>
+            <Input type='number' onChange={handleChange} value={formData} defaultValue={formData}/>
+            <br></br>
+            <SubmitButtonWrapper>
+                <SubmitButton>submit</SubmitButton>
+            </SubmitButtonWrapper>
+            
+        </form>
+        {previousGuess&& <span>
+            The current guess is {previousGuess}
+        </span>}
+        <span>
+            {text}
+        </span>
+        <span>The guess count is {count}</span>
+        <button onClick={handleReset}>reset</button>
+    </GusserWrapper>
+ )
+}
+
+export default Gusser
---
+++ src/Guesser/subcomponents.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const GusserWrapper = styled.div`
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ margin: 24px 48px;
+`
+
+export const Title= styled.span`
+ display: flex;
+ flex-direction: row;
+ justify-content:flex-start;
+`
+
+export const InputWrapper= styled.div`
+ height:24px;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ padding: 24px;
+`
+
+export const Input = styled.input`
+width: 60%;
+height:30px;
+display:flex;
+text-align: center
+`
+
+export const Label = styled.label`
+display:flex;
+font-size:24px;
+font-family: Arial, Helvetica, sans-serif;
+padding:16px;
+text-align: center
+`
+
+export const SubmitButtonWrapper= styled.div`
+
+`
+
+export const SubmitButton = styled.button`
+color:blue;
+`
